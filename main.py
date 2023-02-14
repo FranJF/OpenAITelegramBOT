a30b6652eef8c36f02216c2f83b7ad004edd5392
Francisco Javier Castillo Fernández <franjf@pc-143-232.customer.ask4.lan>
2023-02-14 13:59:03 +0100

    fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import openai
 
 openai.api_key = "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"
-completion = openai.Completion.create(engine="text-davinci-003", prompt="", max_tokens=2048)
+completion = openai.Completion.create(engine="text-davinci-003", prompt="¿Quien es el presidente de Estados Unidos?", max_tokens=2048)
 respuesta:str = completion.choices[0].text
 print(respuesta)
