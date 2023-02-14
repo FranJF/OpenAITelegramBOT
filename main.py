339ac8aaa7b8b869f6c4c8a7562603802d5e8561
Francisco Javier Castillo Fernández <franjf@pc-143-232.customer.ask4.lan>
2023-02-14 14:12:59 +0100

    telegram

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import openai
+import telegram
 
-openai.api_key = "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"
-completion = openai.Completion.create(engine="text-davinci-003", prompt="¿Quien es el presidente de Estados Unidos?", max_tokens=2048)
+pregunta:str = "¿Quien es el presidente de Estados Unidos?"
+
+openai.api_key = "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"
+completion = openai.Completion.create(engine="text-davinci-003", prompt=pregunta, max_tokens=2048)
 respuesta:str = completion.choices[0].text
 print(respuesta)
+
+
+
+
+TELEGRAM_BOT_KEY_API = "6247474269:AAH4Jh8GNrQw8HWFXScsTIsWnAtJUdtuDKw"
+
+async def main():
+    bot = telegram.Bot(TELEGRAM_BOT_KEY_API)
+    async with bot:
+        update = await bot.get_updates()[0]
+        chat_id:int = update.message.from_user.id
+        await bot.send_message(text='Hi John!', chat_id=chat_id)
