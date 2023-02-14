678a6bee096119e830522a2e43678e091fa4a2d7
Francisco Javier Castillo Fernández <franjf@pc-143-232.customer.ask4.lan>
2023-02-14 13:49:03 +0100

    fix

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import OPENAI_API_KEY
 import openai
 
-openai.api_key = OPENAI_API_KEY
+openai.api_key = "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"
 completion = openai.Completion.create(engine="text-davinci-003", prompt="", max_tokens=2048)
 respuesta:str = completion.choices[0].text
 print(respuesta)
