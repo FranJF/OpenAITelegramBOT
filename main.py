a1359b2e32927893aa7cdedf6d708c7ce3b739c9
FranJF <franjf@pc-143-232.customer.ask4.lan>
2023-02-16 18:03:45 +0100

    fix

--- main.py
+++ main.py
@@ -12,9 +12,6 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # pregunta:str = "¿Quien es el presidente de Estados Unidos?"
 
-# openai.api_key = OPENAI_API_KEY
-# completion = openai.Completion.create(engine="text-davinci-003", prompt=pregunta, max_tokens=2048)
-# respuesta:str = completion.choices[0].text
 # print(respuesta)
 async def main() -> None:
     """Run the bot."""
@@ -37,6 +34,10 @@ async def main() -> None:
                 # The user has removed or blocked the bot.
                 update_id += 1
 
+async def get_respuesta_openai(pregunta:str):
+    openai.api_key = OPENAI_API_KEY
+    completion = openai.Completion.create(engine="text-davinci-003", prompt=pregunta, max_tokens=2048)
+    return completion.choices[0].text
 
 async def echo(bot, update_id: int) -> int:
     """Echo the message the user sent."""
@@ -44,13 +45,10 @@ async def echo(bot, update_id: int) -> int:
     updates = await bot.get_updates(offset=update_id, timeout=10)
     for update in updates:
         next_update_id = update.update_id + 1
-
-        # your bot can receive updates without messages
-        # and not all messages contain text
         if update.message and update.message.text:
-            # Reply to the message
-            # logger.info("Found message %s!", update.message.text)
-            await update.message.reply_text(update.message.text)
+            pregunta:str = update.message.text
+            respuesta_bot_ia = await get_respuesta_openai(pregunta)
+            await update.message.reply_text(respuesta_bot_ia)
         return next_update_id
     return update_id
 
