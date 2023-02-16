9644a705c66b68720a914c34f1c35db1533bd8d0
FranJF <franjf@pc-143-232.customer.ask4.lan>
2023-02-16 17:58:09 +0100

    fix

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import asyncio
 import openai
 import telegram
+from telegram.error import Forbidden, NetworkError
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,12 +16,43 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 # completion = openai.Completion.create(engine="text-davinci-003", prompt=pregunta, max_tokens=2048)
 # respuesta:str = completion.choices[0].text
 # print(respuesta)
+async def main() -> None:
+    """Run the bot."""
+    # Here we use the `async with` syntax to properly initialize and shutdown resources.
+    async with telegram.Bot(TELEGRAM_BOT_KEY_API) as bot:
+        # get the first pending update_id, this is so we can skip over it in case
+        # we get a "Forbidden" exception.
+        try:
+            update_id = (await bot.get_updates())[0].update_id
+        except IndexError:
+            update_id = None
 
-async def main():
-    bot = telegram.Bot(TELEGRAM_BOT_KEY_API)
-    while True:
-        async with bot:
-            chat_id = (await bot.get_updates())[0].message.from_user.id
-            await bot.send_message(text='Hi John!', chat_id=chat_id)
+        # logger.info("listening for new messages...")
+        while True:
+            try:
+                update_id = await echo(bot, update_id)
+            except NetworkError:
+                await asyncio.sleep(1)
+            except Forbidden:
+                # The user has removed or blocked the bot.
+                update_id += 1
+
+
+async def echo(bot, update_id: int) -> int:
+    """Echo the message the user sent."""
+    # Request updates after the last update_id
+    updates = await bot.get_updates(offset=update_id, timeout=10)
+    for update in updates:
+        next_update_id = update.update_id + 1
+
+        # your bot can receive updates without messages
+        # and not all messages contain text
+        if update.message and update.message.text:
+            # Reply to the message
+            # logger.info("Found message %s!", update.message.text)
+            await update.message.reply_text(update.message.text)
+        return next_update_id
+    return update_id
+
 
 asyncio.run(main())
